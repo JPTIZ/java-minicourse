21a7452ff15958147e2b6243285bc45f8639a6bc
JPTIZ <jpaulotiz@gmail.com>
2017-05-26 20:40:16 -0300

    updating shop with no quantity in product

--- 4_classes_e_boas_praticas/shop/Product.java
+++ 4_classes_e_boas_praticas/shop/Product.java
@@ -1,35 +1,18 @@
+
 public class Product {
-    private String name;
-    private double price;
-    private int quantity;
-
-    public Product(String name, double price, int quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getPrice() {
-        return this.price;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public int getQuantity() {
-        return this.quantity;
-    }
+	private String name;
+	private int price;
+	
+	public Product(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
 }
