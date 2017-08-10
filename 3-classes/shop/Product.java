86692314bb3309107f3f8fc10c9dfb04e62f56be
JPTIZ <jpaulotiz@gmail.com>
2017-08-10 14:07:25 -0300

    no methods before methods class

--- 3-classes/shop/Product.java
+++ 3-classes/shop/Product.java
@@ -1,18 +1,4 @@
-
 public class Product {
-	private String name;
-	private int price;
-	
-	public Product(String name, int price) {
-		this.name = name;
-		this.price = price;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public int getPrice() {
-		return price;
-	}
+    public String name;
+    public int price;
 }
--- 5-metodos/shop/Application.java
+++ 5-metodos/shop/Application.java
@@ -2,34 +2,27 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        Product[] products = new Product[4];
-
-        products[0].name = "Pão";
-        products[0].price = 50;
-
-        products[1].name = "Leite";
-        products[1].price = 280;
-
-        products[2].name = "Suco";
-        products[2].price = 400;
-
-        products[3].name = "Pizza";
-        products[3].price = 899;
+        Product[] products = {
+                new Product("Pão", 50),
+                new Product("Leite", 280),
+                new Product("Suco", 400),
+                new Product("Pizza", 899),
+        };
 
         int[] amounts = new int[4];
         Scanner reader = new Scanner(System.in);
 
         for (int i = 0; i < products.length; i++) {
             System.out.printf("Digite a quantidade de %s",
-                              products[i].name);
+                              products[i].getName());
             amounts[i] = reader.nextInt();
         }
 
         int total = 0;
         for (int i = 0; i < products.length; i++) {
-            int totalProduct = amounts[i] * products[i].price;
+            int totalProduct = amounts[i] * products[i].getPrice();
             System.out.printf("Total gasto com %s: R$%d\n",
-                              products[i].name,
+                              products[i].getName(),
                               totalProduct);
             total += totalProduct;
         }
