294ba3d975d3796c6852a94d4a8cef998131e923
JPTIZ <jpaulotiz@gmail.com>
2017-08-10 13:52:23 -0300

    it's price, not cost

--- 1-intro-ifs/shop/Main.java
+++ 1-intro-ifs/shop/Main.java
@@ -10,10 +10,10 @@ public class Main {
         // Para nossa loja, precisamos dos preços de cada produto.
         // Como dinheiro é um número real (com vírgula), utiliza-se o tipo
         // "double" para representar os valores dos preços.
-        double breadCost = 0.5;
-        double milkCost = 2.6;
-        double juiceCost = 4.0;
-        double pizzaCost = 7.99;
+        double breadPrice = 0.5;
+        double milkPrice = 2.6;
+        double juicePrice = 4.0;
+        double pizzaPrice = 7.99;
 
         // Aqui ficam as quantidades que o cliente comprou de cada produto
         int breadAmount = 4;
@@ -29,25 +29,26 @@ public class Main {
         if (breadAmount > 0) {
             // Se foi comprado pão, calculamos o quanto foi gasto com eles
             // (preço * quantidade)
-            double totalBread = breadCost * breadAmount;
+            double totalBread = breadPrice * breadAmount;
             // Exibimos (para o cliente ter os dados detalhados)
             System.out.println("Total gasto com pães: R$" + totalBread);
             // Somamos o gasto com pães ao resto da compra
             total += totalBread;
         }
+
         // O mesmo é feito para o leite, suco e pizza
         if (milkAmount > 0) {
-            double totalMilk = milkCost * milkAmount;
+            double totalMilk = milkPrice * milkAmount;
             System.out.println("Total gasto com leite: R$" + totalMilk);
             total += totalMilk;
         }
         if (juiceAmount > 0) {
-            double totalJuice = juiceCost * juiceAmount;
+            double totalJuice = juicePrice * juiceAmount;
             System.out.println("Total gasto com suco: R$" + totalJuice);
             total += totalJuice;
         }
         if (pizzaAmount > 0) {
-            double totalPizza = pizzaCost * pizzaAmount;
+            double totalPizza = pizzaPrice * pizzaAmount;
             System.out.println("Total gasto com pizzas: R$" + totalPizza);
             total += totalPizza;
         }
