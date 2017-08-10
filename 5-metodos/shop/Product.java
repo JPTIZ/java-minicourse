3a36e11cfd5a1b34cc1b3e1d1692364fc39fd6e1
JPTIZ <jpaulotiz@gmail.com>
2017-08-10 14:07:25 -0300

    no methods before methods class

---
+++ 5-metodos/shop/Product.java
@@ -0,0 +1,18 @@
+
+public class Product {
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    private String name;
+    private int price;
+}
