ca739afd0afb4ad46b5c23f453a88dcfef4b5276
JPTIZ <jpaulotiz@gmail.com>
2017-08-10 22:24:06 -0300

    fixed generic warning and stack generics

--- 8-estruturas-de-dados/DataStructures/src/ArrayList.java
+++ 8-estruturas-de-dados/DataStructures/src/ArrayList.java
@@ -1,29 +1,30 @@
-
 public class ArrayList<T> {
-	public void add(T value) {
-		if (size == data.length) {
-			T[] newData = (T[])new Object[data.length * 2];
-			for (int i = 0; i < size; i++) {
-				newData[i] = data[i];
-			}
-			data = newData;
-		}
-		data[size] = value;
-		++size;
-	}
-	
-	public void remove(int index) {
-		
-	}
-	
-	public T at(int index) {
-		return data[index];
-	}
-	
-	public int size() {
-		return size;
-	}
-	
-	private T[] data = (T[])new Object[4];
-	private int size = 0;
+    public void add(T value) {
+        if (size == data.length) {
+            @SuppressWarnings("unchecked")
+            T[] newData = (T[])new Object[data.length * 2];
+            for (int i = 0; i < size; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+        data[size] = value;
+        ++size;
+    }
+
+    public void remove(int index) {
+
+    }
+
+    public T at(int index) {
+        return data[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @SuppressWarnings("unchecked")
+    private T[] data = (T[])new Object[4];
+    private int size = 0;
 }
--- 8-estruturas-de-dados/DataStructures/src/LinkedStack.java
+++ 8-estruturas-de-dados/DataStructures/src/LinkedStack.java
@@ -1,16 +1,16 @@
 
-public class LinkedStack {
-	public void push(int value) {
-		contents.insert(value, 0);
-	}
-	
-	public void pop() {
-		contents.remove(0);
-	}
-	
-	public int size() {
-		return contents.size();
-	}
-	
-	private LinkedList contents = new LinkedList<>();
+public class LinkedStack<T> {
+    public void push(T value) {
+        contents.insert(value, 0);
+    }
+
+    public void pop() {
+        contents.remove(0);
+    }
+
+    public int size() {
+        return contents.size();
+    }
+
+    private LinkedList<T> contents = new LinkedList<>();
 }
