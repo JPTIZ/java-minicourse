d6356b78c8c579d33d1eccaeeaa43c76abad96a2
JPTIZ <jpaulotiz@gmail.com>
2017-08-10 21:37:15 -0300

    add class incomplete example

---
+++ 6-projetos/Shop/src/MainMenuView.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainMenuView {
+	
+	public int showOptions() {
+		System.out.print(
+				"Selecione uma opção:\n" +
+				"1 - Cadastrar produto\n" +
+				"2 - Iniciar venda\n" +
+				"3 - Listar produtos\n" +
+				"4 - Ver relatório\n" +
+				"5 - Sair\n" +
+				"Resposta:"
+		);
+		return reader.nextInt();
+	}
+	
+	public String askProductName() {
+		System.out.print("Nome do produto: ");
+		return reader.next();
+	}
+	
+	public int askProductPrice() {
+		System.out.print("Preço do produto: ");
+		return reader.nextInt();
+	}
+
+	public void listProducts(ArrayList<Product> products) {
+		for (Product product: products) {
+			System.out.println(product.getName());
+		}
+	}
+	
+	public void showWarning(String message) {
+		System.out.println(message);
+	}
+	
+	private Scanner reader = new Scanner(System.in);
+}
---
+++ 6-projetos/Shop/src/SaleView.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SaleView {
+	public int showOptions() {
+		System.out.print(
+				"Selecione uma opção: \n" +
+				"1 - Adicionar produto\n" +
+				"2 - Remover produto\n" +
+				"3 - Finalizar venda\n" +
+				"4 - Cancelar venda\n" +
+				"Resposta: "
+			);
+		
+		return reader.nextInt();
+	}
+	
+	public int askProduct() {
+		System.out.println("Código do produto: ");
+		return reader.nextInt();
+	}
+	
+	public void listProducts(ArrayList<Product> products) {
+		System.out.println("Cód. | Nome");
+		for (int i = 0; i < products.size(); i++) {
+			Product product = products.get(i);
+			System.out.printf(
+					" %3d | %s \n",
+					i,
+					product.getName());
+		}
+	}
+	
+	public int askAmount() {
+		System.out.print("Quantidade: ");
+		return reader.nextInt();
+	}
+	
+	public void showWarning(String message) {
+		System.out.println("Erro: " + message);
+	}
+	
+	private Scanner reader = new Scanner(System.in); 
+}
