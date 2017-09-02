d6bde1801993115c11aa1df9cbc64af8efb99675
JPTIZ <jpaulotiz@gmail.com>
2017-09-02 16:03:34 -0300

    better explanation and method names

--- 5-metodos/explanation/Main.java
+++ 5-metodos/explanation/Main.java
@@ -13,13 +13,13 @@
 public class Main {
     // Para não ficarmos repetindo as operações de mostrar o conteúdo dos
     // vetores (que é a mesma, independente de qual vetor seja), criaremos um
-    // método chamado `showArray`.
+    // método chamado `show`.
     // Perceba que ele está marcado como `public` e `static`, assim como o
     // `main`. Por enquanto teremos que ignorar estas palavrinhas por enquanto,
     // mas elas farão mais sentido na próxima aula (nº 4).
     //
-    // Mas já fica de reforço:
-    //  - Métodos `static` podem chamar SOMENTE métodos estáticos.
+    // Mas já fica de reforço: - Métodos `static` podem chamar SOMENTE métodos
+    // estáticos.
     //
     // Métodos são declarados da seguinte forma:
     //
@@ -30,51 +30,48 @@ public class Main {
     // Sendo:
     //
     //   TipoDeRetorno:
-    //     Significa qual o tipo de dado que será retornado pelo
-    //         método. Um método "somaInt", por exemplo, irá somar
-    //         dois `int`s e retornar essa soma. Nesse caso, o tipo
-    //         de retorno será `int`. Um método "raizQuadrada",
-    //         porém, recebe um número e retorna a raiz quadrada
-    //         dele. Como raízes quadradas são números reais, o tipo
-    //         de retorno seria um `double` (ou `float`, dependendo
-    //         do que o programador decidir).
-    //       No nosso caso, não queremos retornar nada, afinal, só
-    //         queremos exibir os valores do vetor, e nada mais. Para
-    //         esse caso (em que nenhum dado será retornado pelo
-    //         método) existe o tipo de retorno `void`, que quer
-    //         dizer "vazio", ou seja, nada será retornado.
+    //     Significa qual o tipo de dado que será retornado pelo método. Um
+    //         método "somaInt", por exemplo, irá somar dois `int`s e retornar
+    //         essa soma. Nesse caso, o tipo de retorno será `int`. Um método
+    //         "raizQuadrada", porém, recebe um número e retorna a raiz
+    //         quadrada dele. Como raízes quadradas são números reais, o tipo
+    //         de retorno seria um `double` (ou `float`, dependendo do que o
+    //         programador decidir).
+    //       No nosso caso, não queremos retornar nada, afinal, só queremos
+    //         exibir os valores do vetor, e nada mais. Para esse caso (em que
+    //         nenhum dado será retornado pelo método) existe o tipo de retorno
+    //         `void`, que quer dizer "vazio", ou seja, nada será retornado.
     //
     //   Nome:
     //     É o nome do método propriamente dito, por exemplo: "println",
-    //       "main", e no nosso caso, `showArray` e "invertArray".
-    //     OBS: Tente sempre ser o mais intuitivo o possível ao dar nomes
-    //       aos métodos. Dar nomes como "x", "aux" e "sV" (na tentativa de
-    //       encurtar `showArray`) dificultam a __manutenção__ do código,
-    //       principalmente se outras pessoas forem utilizar um método que
-    //       você tenha criado. Tente pensar por exemplo se o método
-    //       "println" se chamasse "pln": qualquer pessoa que lesse o
-    //       código não saberia dizer o que o método faz (nem chutar qual
-    //       seria sua utilidade), e a última coisa que ela pensaria é que
-    //       o método simplesmente mostra no console o que estiver entre
-    //       os parênteses.
+    //       "main", e no nosso caso, `show` e "reverse".
+    //     OBS: Tente sempre ser o mais intuitivo o possível ao dar nomes aos
+    //       métodos. Dar nomes como "x", "aux" e "sV" (na tentativa de
+    //       encurtar `show`) dificultam a __manutenção__ do código,
+    //       principalmente se outras pessoas forem utilizar um método que você
+    //       tenha criado. Tente pensar por exemplo se o método "println" se
+    //       chamasse "pln": qualquer pessoa que lesse o código não saberia
+    //       dizer o que o método faz (nem chutar qual seria sua utilidade), e
+    //       a última coisa que ela pensaria é que o método simplesmente mostra
+    //       no console o que estiver entre os parênteses.
     //
     //   Parâmetros:
     //     São opcionais (basta deixar os parênteses vazios).
-    //     No caso do método `showArray`, nenhuma linguagem de
-    //       programação vai magicamente advinhar qual que é o vetor
-    //       que será exibido quando o método for chamado. Ou seja,
-    //       precisamos dizer ao método qual é o vetor que será exibido, e para
-    //       isso enviamos o vetor nos parênteses do método. Portanto o vetor é
-    //       chamado de "parâmetro" do método.
+    //     No caso do método `show`, nenhuma linguagem de programação vai
+    //       magicamente advinhar qual que é o vetor que será exibido quando o
+    //       método for chamado. Ou seja, precisamos dizer ao método qual é o
+    //       vetor que será exibido, e para isso enviamos o vetor nos
+    //       parênteses do método. Portanto o vetor é chamado de "parâmetro" do
+    //       método.
     //     Os parâmetros do método são designados como se fosse uma declaração:
     //       tipo nome
     //     Portanto, se nosso método irá receber um vetor de inteiros,
     //       definimos o parâmetro como:
     //         `int[] array`
-    //     Assim, quem quiser utilizar o método `showArray` precisará enviar
-    //       um vetor de inteiros dentro dos parênteses.
+    //     Assim, quem quiser utilizar o método `show` precisará enviar um
+    //       vetor de inteiros dentro dos parênteses.
     //
-    public static void showArray(int[] array) {
+    public static void show(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + ", ");
         }
@@ -82,19 +79,16 @@ public class Main {
     }
 
     // No método que inverte os vetores, acontecerá a mesma coisa que para o
-    // `showArray`: receberemos um vetor de inteiros, e dentro dele iremos
-    // inverter os vetores.
-    public static void invertArray(int[] array) {
+    // `show`: receberemos um vetor de inteiros, e dentro dele iremos inverter
+    // os vetores.
+    public static void reverse(int[] array) {
         for (int i = 0; i < array.length/2; i++) {
-            // Como iremos alterar array[i], precisamos
-            // de uma cópia do valor antigo (para colocar
-            // no lado oposto do vetor).
+            // Como iremos alterar array[i], precisamos de uma cópia do valor
+            // antigo (para colocar no lado oposto do vetor).
             int swap = array[i];
-            // Atribuimos o valor no
-            // lado oposto do vetor em array[i].
+            // Atribuimos o valor no lado oposto do vetor em array[i].
             array[i] = array[array.length - 1 - i];
-            // Atribuimos o antigo valor
-            // de array[i] na outra ponta do vetor.
+            // Atribuimos o antigo valor de array[i] na outra ponta do vetor.
             array[array.length - 1 - i] = swap;
         }
     }
@@ -111,32 +105,32 @@ public class Main {
         System.out.println("Vetores (antes):");
 
         // Assim como fazemos com println(...), aqui estamos invocando o método
-        // chamado `showArray`, que recebe, por parâmetro, um vetor (cujo
-        // conteúdo será exibido).
-        showArray(array1);
-        showArray(array2);
-        showArray(array3);
-        showArray(array4);
+        // chamado `show`, que recebe, por parâmetro, um vetor (cujo conteúdo
+        // será exibido).
+        show(array1);
+        show(array2);
+        show(array3);
+        show(array4);
 
         // Agora basta chamar o método que inverte um vetor para cada um dos 4
         // vetores instanciados anteriormente.
-        invertArray(array1);
-        invertArray(array2);
-        invertArray(array3);
-        invertArray(array4);
+        reverse(array1);
+        reverse(array2);
+        reverse(array3);
+        reverse(array4);
 
         System.out.println("Vetores (invertidos):");
 
         // E por fim chamar novamente o método de exibir um vetor.
-        showArray(array1);
-        showArray(array2);
-        showArray(array3);
-        showArray(array4);
+        show(array1);
+        show(array2);
+        show(array3);
+        show(array4);
 
-        // Perceba que desta forma podemos usar `showArray` onde quisermos,
-        // sem a necessidade de reescrever todos os passos dele. Essa é uma das
+        // Perceba que desta forma podemos usar `show` onde quisermos, sem a
+        // necessidade de reescrever todos os passos dele. Essa é uma das
         // vantagens de se utilizar métodos, além de que fica mais organizado:
         // não é preciso ler o que cada `for` está fazendo para saber que se
-        // está invertendo um vetor (ou exibindo ele, no caso de `showArray`).
+        // está invertendo um vetor (ou exibindo ele, no caso de `show`).
     }
 }
--- 5-metodos/extra/MethodFail.java
+++ 5-metodos/extra/MethodFail.java
@@ -1,27 +1,67 @@
+/**
+ * Este exemplo serve para mostrar um pouco sobre passagem de argumentos por
+ * referência e por valor.
+ *
+ * Tente executar o código, cheque o texto que aparece no console e tente
+ * entender as explicações.
+ */
 public class MethodFail {
-
+    /**
+     * Tipo aleatório apenas para testes.
+     * O objetivo é verificar quando o membro `x` é alterado.
+     */
     public static class MyType {
         public int x;
     }
 
+    /**
+     * Tenta alterar `foo` para uma nova instância de `MyType`.
+     * Perceba que o endereço printado é diferente nos dois `foo` aqui, mas o
+     * `foo` de `main` continua o mesmo.
+     *
+     * Isso se dá porque em `Java` toda variável que deveria guardar um objeto
+     * na verdade guarda qual endereço de memória ela referencia. Assim,
+     * recebemos por parâmetro um endereço de memória e *copiamos* o endereço
+     * (não o objeto!) para `foo`. Quando é feito `foo = new MyType();`, apenas
+     * estamos alterando para quem o parâmetro `foo` aponta, mas o `foo` de
+     * quem chamou o método `change` continua intacto.
+     */
+    public static void change(MyType foo) {
+        System.out.println("Foo (Change - antes): " + foo);
+        foo = new MyType();
+        System.out.println("Foo (Change - depois): " + foo);
+    }
+
+    /**
+     * Tenta elevar o `value` passado ao cubo.
+     * Perceba, porém, que ele é apenas alterado aqui. Isso acontece porque
+     * tipos primitivos são copiados quando passados por parâmetro. Assim, o
+     * `value` que trabalhamos aqui não é o mesmo `value` passado por parâmetr
+     * por que mchamou `cubic`.
+     */
+    public static void cubic(int value) {
+        value = value * value * value;
+    }
+
+    /**
+     * Chama alguma das funções anteriores passando variáveis como parâmetro, a
+     * fim de checar o que acontece com elas.
+     */
     public static void main(String[] args) {
         MyType foo = new MyType();
 
-        System.out.println(foo);
+        System.out.println("Foo (Main): " + foo);
 
         foo.x = 10;
         change(foo);
 
-        System.out.println(foo);
-    }
+        System.out.println("Foo (Main - após change): " + foo);
 
-    public static void change(MyType foo) {
-        System.out.println(foo);
-        foo = new MyType();
-        System.out.println(foo);
-    }
+        int value = 10;
+        System.out.println("Value (Main - antes de cubic): " + value);
 
-    public static void cubic(int value) {
-        value = value * value * value;
+        cubic(value);
+
+        System.out.println("Value (Main - após cubic): " + value);
     }
 }
